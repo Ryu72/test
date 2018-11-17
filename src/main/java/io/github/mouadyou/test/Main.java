9ef85d8e7e0cc7b9d9dbf9ae58940013cd63fa41
Youssef Mouad <youssef-mouad@github.com>
2018-11-17 18:07:45 +0100

    Changing test.Main.java

--- src/main/java/io/github/mouadyou/test/Main.java
+++ src/main/java/io/github/mouadyou/test/Main.java
@@ -2,6 +2,6 @@ package io.github.mouadyou.test;
 
 public class Main() {
     public static void main(String args[]) {
-        System.out.printLn("Nani the fuck !!");
+        System.out.printLn("Dude What !!");
     }
 }
